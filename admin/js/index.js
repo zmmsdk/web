3b3b9750980835692dbe726c29f53b78bb7a4832
zmmsdk <a972332466@qq.com>
2019-11-20 21:39:21 +0800

    月新增文章数完成

--- admin/js/index.js
+++ admin/js/index.js
@@ -5,10 +5,7 @@ console.log($logout);
 // 获取用户信息
 var $userImg = $(".userImg");
 var $userName = $('#userName');
-// 获取总文章数和日更新文章数
-var $all_count =$('#all_count');
-var $day_count = $('#day_count');
-console.log($all_count,$day_count);
+
 
 
 $logout.click(function () {
@@ -32,11 +29,4 @@ user.getUser(function (res) {
     }
 });
 
-// 文章数量统计
-$.get('http://localhost:8000/admin/article_count',function(res){
-    console.log(res);
-    if(res.code === 200){
-        $all_count.text(res.data.all_count) ;
-        $day_count.text(res.data.day_count);
-    }
-});
+
---
+++ admin/js/main_count.js
@@ -0,0 +1,219 @@
+// 文章数量统计
+// 获取总文章数和日更新文章数
+var $all_count =$('#all_count');
+var $day_count = $('#day_count');
+// console.log($all_count,$day_count);
+// 获取评论数量
+var $c_all_count =$('#comment_all_count');
+var $c_day_count =$('#comment_day_count');
+// 文章数量统计
+$.get('http://localhost:8000/admin/article_count',function(res){
+    console.log(res);
+    if(res.code === 200){
+        $all_count.text(res.data.all_count) ;
+        $day_count.text(res.data.day_count);
+    }
+});
+
+// 评论数量统计
+$.get('http://localhost:8000/admin/comment_count',function(res){
+    console.log(res);
+    if(res.code === 200){
+        $c_all_count.text(res.data.all_count) ;
+        $c_day_count.text(res.data.day_count);
+    }
+});
+// 月新增文章数
+$.get('http://localhost:8000/admin/month_article_count',function(res){
+    console.log(res);
+    if(res.code === 200){
+        var data = res.data ;
+        console.log(data);
+        var oChart = echarts.init(document.getElementById('curve_show'));
+        var aList_all = data;
+        // [
+        //   {'count':36,'date':'2019-04-13'},
+        //   {'count':52,'date':'2019-04-14'},
+        //   {'count':78,'date':'2019-04-15'},
+        //   {'count':85,'date':'2019-04-16'},
+        //   {'count':65,'date':'2019-04-17'},
+        //   {'count':72,'date':'2019-04-18'},
+        //   {'count':88,'date':'2019-04-19'},
+        //   {'count':64,'date':'2019-04-20'},
+        //   {'count':72,'date':'2019-04-21'},
+        //   {'count':90,'date':'2019-04-22'},
+        //   {'count':96,'date':'2019-04-23'},
+        //   {'count':100,'date':'2019-04-24'},
+        //   {'count':102,'date':'2019-04-25'},
+        //   {'count':110,'date':'2019-04-26'},
+        //   {'count':123,'date':'2019-04-27'},
+        //   {'count':100,'date':'2019-04-28'},
+        //   {'count':132,'date':'2019-04-29'},
+        //   {'count':146,'date':'2019-04-30'},
+        //   {'count':200,'date':'2019-05-01'},
+        //   {'count':180,'date':'2019-05-02'},
+        //   {'count':163,'date':'2019-05-03'},
+        //   {'count':110,'date':'2019-05-04'},
+        //   {'count':80,'date':'2019-05-05'},
+        //   {'count':82,'date':'2019-05-06'},
+        //   {'count':70,'date':'2019-05-07'},
+        //   {'count':65,'date':'2019-05-08'},
+        //   {'count':54,'date':'2019-05-09'},
+        //   {'count':40,'date':'2019-05-10'},
+        //   {'count':45,'date':'2019-05-11'},
+        //   {'count':38,'date':'2019-05-12'},
+        // ];
+
+        let aCount = [];
+        let aDate = [];
+
+        for(var i=0;i<aList_all.length;i++){
+            aCount.push(aList_all[i].day_count);
+            aDate.push(aList_all[i].day);
+        }
+
+        var chartopt = {
+                  title:{
+                      text: '月新增文章数',
+                      left: 'center',
+                      top: '10'
+                  },
+                  tooltip:{
+                      trigger: 'axis'
+                  },
+                  legend: {
+                      data:['新增文章'],
+                      top: '40'
+                  },
+                  toolbox: {
+                      show : true,
+                      feature : {
+                          mark : {show: true},
+                          dataView : {show: true, readOnly: false},
+                          magicType : {show: true, type: ['line','bar']},
+                          restore : {show: true},
+                          saveAsImage : {show: true}
+                      }
+                  },
+                  calculable : true,               
+                  xAxis : [
+                      {
+                          name: '日',
+                          type : 'category',
+                          boundaryGap : false,
+                          data :aDate
+                      }
+                  ],        
+                  yAxis : [
+                      {
+                          name: '月新增文章数',
+                          type : 'value'
+                      }
+                  ],      
+                  series : [
+					        {
+					            name:'新增文章',
+					            type:'line',
+					            smooth:true,
+					            itemStyle: {normal: {areaStyle: {type: 'default'}, color: '#f80'}, lineStyle: {color: '#f80'}},
+					            data:aCount
+                            }],
+                            areaStyle:{
+						        normal:{
+						            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+						                offset: 0,
+						                color: 'rgba(255,136,0,0.39)'
+						            }, {
+						                offset: .34,
+						                color: 'rgba(255,180,0,0.25)'
+						            },{
+						                offset: 1,
+						                color: 'rgba(255,222,0,0.00)'
+						            }])
+
+						        }
+						    },
+                  grid: {
+                      show: true,             
+                      x:50,
+                      x2:50,
+                      y:80,
+                      height:220
+                  }                 
+            };
+
+        oChart.setOption(chartopt);     
+    }
+});
+
+// 各类型文档数量统计
+$.get('http://localhost:8000/admin/article_category_count',function(res){
+    console.log(res);
+    if(res.code === 200){
+        var data = res.data ;
+        // console.log(data);
+        for(var i =0 ;i<data.length;i++){
+            console.log(data[i]);
+            
+        }
+
+        var oPie = echarts.init(document.getElementById('pie_show'));
+        var oPieopt =  
+        {
+          title : {
+              top:10,
+              text: '分类文章数量比',
+              x:'center'
+          },
+          tooltip : {
+              trigger: 'item',
+              formatter: "{a} <br/>{b} : {c} ({d}%)"
+          },
+          color:['#5885e8','#13cfd5','#00ce68','#ff9565'],
+          legend: {
+              x : 'center',
+              top:65,
+              data:['奇趣事','会生活','爱旅行','趣美味']
+          },
+          toolbox: {
+              show : true,
+              x : 'center',
+              top:35,
+              feature : {
+                  mark : {show: true},
+                  dataView : {show: true, readOnly: false},
+                  magicType : {
+                      show: true, 
+                      type: ['pie', 'funnel'],
+                      option: {
+                          funnel: {
+                              x: '25%',
+                              width: '50%',
+                              funnelAlign: 'left',
+                              max: 1548
+                          }
+                      }
+                  },
+                  restore : {show: true},
+                  saveAsImage : {show: true}
+              }
+          },
+          calculable : true,
+          series : [
+              {
+                  name:'访问来源',
+                  type:'pie',
+                  radius: ['45%', '60%'],
+                  center: ['50%', '65%'],
+                  data:[
+                      {value:300, name:'奇趣事'},
+                      {value:100, name:'会生活'},
+                      {value:260, name:'爱旅行'},                   
+                      {value:180,name:'趣美味'}
+                  ]
+              }
+          ]
+      };
+      oPie.setOption(oPieopt);
+    }
+});
