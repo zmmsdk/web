342402d67f45f359eb257f50d9c18fb8c843968a
zmmsdk <a972332466@qq.com>
2019-11-20 20:24:14 +0800

    文章数量统计完成

--- admin/js/index.js
+++ admin/js/index.js
@@ -3,8 +3,13 @@
 var $logout = $('#logout');
 console.log($logout);
 // 获取用户信息
-var userImg = $(".userImg");
-var userName = $('#userName');
+var $userImg = $(".userImg");
+var $userName = $('#userName');
+// 获取总文章数和日更新文章数
+var $all_count =$('#all_count');
+var $day_count = $('#day_count');
+console.log($all_count,$day_count);
+
 
 $logout.click(function () {
     console.log('页面退出成功');
@@ -22,7 +27,16 @@ $logout.click(function () {
 user.getUser(function (res) {
     console.log(res);
     if (res.code === 200) {
-        userImg.prop('src', res.data.user_pic)
-        userName.text(res.data.nickname)
+        $userImg.prop('src', res.data.user_pic)
+        $userName.text(res.data.nickname)
+    }
+});
+
+// 文章数量统计
+$.get('http://localhost:8000/admin/article_count',function(res){
+    console.log(res);
+    if(res.code === 200){
+        $all_count.text(res.data.all_count) ;
+        $day_count.text(res.data.day_count);
     }
 });
