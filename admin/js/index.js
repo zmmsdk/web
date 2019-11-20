5764f8a21093da0dc143619367e4fb948d419014
zmmsdk <a972332466@qq.com>
2019-11-20 18:07:26 +0800

    页面登出

--- admin/js/index.js
+++ admin/js/index.js
@@ -1,7 +1,26 @@
 // index.js  
 // 获取元素
-var logout = $('#logout');
-console.log(logout);
+var $logout = $('#logout');
+console.log($logout);
+$logout.click(function () { 
+    
+    console.log('页面退出成功');
+    // 发请求$.post('地址',参数,回调)
+    $.post('http://localhost:8000/admin/logout',function(res){
+        console.log(res);
+        if(res.code === 200){
+            // 退出回到登录页
+            location.href = './login.html';
+        }
+        
+    })
+
+    
+});
+
+
+
+
 
 
 
