1be7316f0db1b9b900db6eaa8613e3a6a317d730
zmmsdk <you@example.com>
2019-12-03 14:32:34 +0800

    分类导航完成

--- home/js/index.js
+++ home/js/index.js
@@ -1,35 +1,53 @@
-var index_search={
+  // 发请求显示页面内容
+  index_search.in_search(function (res) {
+    // console.log(11)
+    console.log(res);
+    // res = html(res)
+
+   // 使用模板引擎 
+   var htmlStr = template("temp_foucs",res)
+
+    // console.log(htmlStr);
+    $("#focus_list").html(htmlStr)
+
+     // 最新资讯
+     var htmlStr = template('temp_new', res)
+    console.log(htmlStr);
+    $('#new').html(htmlStr)
+
+})
+
+
+index_search.category_show( function (res) {
+    console.log(res);
+    // var strHtml = template('模板id',res)
+    var htmlStr = template('temp_category_search', res)
+    // console.log(htmlStr)
+    $('#level_two').html('    <li class="up"></li>' + htmlStr)
+
+    $('#left_menu').html(htmlStr)
+})
+
+
+// 文章热门排行
+index_search.in_rank( function (res) {
+    console.log(res);
+    // var strHtml = template('模板id',res)
+    var htmlStr = template('temp_hot', res)
+    // console.log(htmlStr)
  
-    category_show:(callback) =>{
-        $.get(APIURLS.category_show,
-            
-            function(res){
-            callback(res)
-        })
-    },
-    in_search:callback =>{
-        $.get( APIURLS.article_show,{
-            perpage: 5,
-            state: '已发布'
-        },res=>{
-           callback(res)
-            
-        })
-    },
-    in_rank:callback =>{
-        $.get( APIURLS.rank,{
-            perpage: 5,
-            state: '已发布'
-        },res=>{
-           callback(res)
-            
-        })
-    },
-    latest_com:callback =>{
-        $.get( APIURLS.latest_comment,res=>{
-           callback(res)
-            
-        })
-    },
-  
-}
+
+    $('#content_list').html(htmlStr)
+})
+
+
+// 最新评论
+index_search.latest_com( function (res) {
+    console.log(res);
+    // var strHtml = template('模板id',res)
+    var htmlStr = template('temp_com', res)
+    console.log(htmlStr)
+ 
+
+    $('#comment_list ').html(htmlStr)
+})
---
+++ home/js/utils/index.js
@@ -0,0 +1,35 @@
+var index_search={
+ 
+    category_show:(callback) =>{
+        $.get(APIURLS.category_show,
+            
+            function(res){
+            callback(res)
+        })
+    },
+    in_search:callback =>{
+        $.get( APIURLS.article_show,{
+            perpage: 5,
+            state: '已发布'
+        },res=>{
+           callback(res)
+            
+        })
+    },
+    in_rank:callback =>{
+        $.get( APIURLS.rank,{
+            perpage: 5,
+            state: '已发布'
+        },res=>{
+           callback(res)
+            
+        })
+    },
+    latest_com:callback =>{
+        $.get( APIURLS.latest_comment,res=>{
+           callback(res)
+            
+        })
+    },
+  
+}
